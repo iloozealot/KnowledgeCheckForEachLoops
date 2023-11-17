7091000f31f2041351e4393a2a55aa98f54dd5a2
iloozealot <iloozealot@gmail.com>
2023-11-17 12:36:18 -0600

    Add project files.

---
+++ ForEachLoops/Program.cs
@@ -0,0 +1,43 @@
+﻿
+//Console application?
+//Ask user for first name?
+//Keep asking for first names until no more?
+//for each name say hello on the console?
+
+List<string> names = new List<string>();
+string firstName = string.Empty;
+bool input = false;
+
+do
+{
+    Console.WriteLine();
+    Console.Write("What is your first name: ");
+	firstName = Console.ReadLine();
+	if (firstName != string.Empty || firstName != null)
+	{
+		names.Add(firstName);
+	}
+    Console.WriteLine();
+    Console.Write("Is there another first name y/n: ");
+
+	if (Console.ReadLine() == "y")
+	{
+		input = true;
+    }
+	else
+	{
+		foreach (string name in names)
+		{
+			Console.WriteLine();
+            Console.WriteLine($"Hello {name}");
+        }
+
+		break;
+	}
+} while (input == true);
+
+
+
+
+
+
